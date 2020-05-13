1fe86c8a9bd5683f58ca734dcf856278ad8ef667
Yana1994ya <yana11114@gmail.com>
2020-05-13 20:34:23 +0300

    Test: Add unit and integration tests.
    Tag the tests according to the Jira tasks.

--- centers/tests.py
+++ centers/tests.py
@@ -6,12 +6,17 @@ from centers.models import City, Neighborhood, Network, Center
 
 class ModelsTestCase(TestCase):
     # unit tests
+
+    # Unit test #1 - Yana
+    # PM2020T15-34
     def test_neighborhood_visible(self):
         city = City(name="Tel Aviv")
         neighborhood = Neighborhood(city=city, name="Center", visible=False)
 
         self.assertTrue("Center" not in str(neighborhood))
 
+    # Unit test #2 - Yana
+    # PM2020T15-34
     def test_city_name_property(self):
         city = City(name="Tel Aviv")
         neighborhood = Neighborhood(city=city, name="Center", visible=False)
@@ -28,33 +33,115 @@ class ModelsTestCase(TestCase):
 
         self.assertEqual(center.city_name, "Tel Aviv")
 
-    # Integration tests
-    def test_search_by_city(self):
-        city_tel_aviv = City(name="Tel Aviv")
-        city_tel_aviv.save()
-        city_ashdod = City(name="Ashdod")
-        city_ashdod.save()
 
-        neighborhood_tel_aviv = Neighborhood(city=city_tel_aviv, name="Center", visible=False)
-        neighborhood_tel_aviv.save()
+# Integration tests
+class ModelsIntegrationTestCase(TestCase):
+    def setUp(self) -> None:
+        self.city_tel_aviv = City(name="Tel Aviv")
+        self.city_tel_aviv.save()
 
-        network = Network(name="Macabbi")
-        network.save()
+        self.city_ashdod = City(name="Ashdod")
+        self.city_ashdod.save()
 
-        Center(
+        self.neighborhood_tel_aviv = Neighborhood(city=self.city_tel_aviv, name="Center", visible=False)
+        self.neighborhood_tel_aviv.save()
+
+        self.network = Network(name="Macabbi")
+        self.network.save()
+
+    def add_center1(self) -> Center:
+        center = Center(
             name="Gahha",
-            neighborhood=neighborhood_tel_aviv,
+            neighborhood=self.neighborhood_tel_aviv,
             address="Sdarot Moshea Dayan 13",
-            network=network,
+            network=self.network,
             hours="09:00-17:00",
             phone="04-23467890",
             fax="04-23467890",
             email="test@test.com",
             routes="Bus 12"
-        ).save()
+        )
 
-        # test if we can find the center in Tel Aviv
-        self.assertTrue(Center.objects.filter(neighborhood__city=city_tel_aviv).exists())
+        center.save()
 
-        # test if the center is not visible for a search in Ashdod
-        self.assertFalse(Center.objects.filter(neighborhood__city=city_ashdod).exists())
+        return center
+
+    def add_center2(self) -> Center:
+        center = Center(
+            name="Wall",
+            neighborhood=self.neighborhood_tel_aviv,
+            address="Herzel 1",
+            network=self.network,
+            hours="09:00-17:00",
+            phone="04-57421812",
+            fax="04-23567868",
+            email="test2@test2.com",
+            routes="Bus 35"
+        )
+
+        center.save()
+
+        return center
+
+    # Integration test #1 - Yana
+    # PM2020T15-34
+    def test_search_by_city(self):
+        # function 1 - save the center into the database
+        self.add_center1()
+
+        # function 2 - test if we can find the center in Tel Aviv
+        self.assertTrue(Center.objects.filter(neighborhood__city=self.city_tel_aviv).exists())
+
+        # function 3 - test if the center is not visible for a search in Ashdod
+        self.assertFalse(Center.objects.filter(neighborhood__city=self.city_ashdod).exists())
+
+    # Integration test #2 - Yana
+    # PM2020T15-34
+    def test_delete_a_center(self):
+        # function 1 - save the center into the database
+        center = self.add_center1()
+
+        # function 2 - test if we can find the center in Tel Aviv
+        self.assertTrue(Center.objects.filter(neighborhood__city=self.city_tel_aviv).exists())
+
+        # function 3 - delete the center
+        center.delete()
+
+        # function 4 - test that we can no longer find the center in Tel Aviv
+        self.assertFalse(Center.objects.filter(neighborhood__city=self.city_tel_aviv).exists())
+
+    # Integration test #3 - Yana
+    # PM2020T15-34
+    def test_find_centers_by_neighborhood(self):
+        # function 1 - save the center1 into the database
+        self.add_center1()
+
+        # function 2 - save the center2 into the database
+        self.add_center2()
+
+        # function 3 - test that both centers are accessible via neighborhood
+        self.assertEqual(self.neighborhood_tel_aviv.center_set.count(), 2)
+
+    # Integration test #4 - Yana
+    # PM2020T15-34
+    def test_find_centers_by_network(self):
+        # function 1 - save the center1 into the database
+        self.add_center1()
+
+        # function 2 - save the center2 into the database
+        self.add_center2()
+
+        # function 3 - test that both centers are accessible via neighborhood
+        self.assertEqual(self.network.center_set.count(), 2)
+
+    # Integration test #5 - Yana
+    # PM2020T15-34
+    def test_find_centers_by_city(self):
+        # function 1 - save the center1 into the database
+        self.add_center1()
+
+        # function 2 - save the center2 into the database
+        self.add_center2()
+
+        # function 3 - test that both centers are accessible via city
+        self.assertEquals(Center.objects.filter(neighborhood__city=self.city_tel_aviv).count(), 2)
--- users/tests.py
+++ users/tests.py
@@ -1,5 +1,5 @@
 from django.forms import ValidationError
-from django.http import HttpRequest , HttpResponseRedirect
+from django.http import HttpRequest, HttpResponseRedirect
 from django.test import TestCase
 
 from users.forms import RegistrationForm
@@ -8,142 +8,174 @@ from users.views import register
 
 
 # Create your tests here.
-class RegistrationTests (TestCase):
+class RegistrationTests(TestCase):
     def setUp(self):
-        Customer.objects.create(username='123456780') 
-        
-    # Unit tests:
-    
+        Customer.objects.create(username='123456780')
+
+        # Unit tests:
+
+    # Unit test #1 - Yana
+    # PM2020T15-40
     def test_validate_citizen_id_number(self):
         form = RegistrationForm()
-        form.cleaned_data = {'id_number':'123456789'}
+        form.cleaned_data = {'id_number': '123456789'}
         result = form.clean_id_number()
         self.assertEqual(result, '123456789')
-        
+
+    # Unit test #2 - Yana
+    # PM2020T15-40
     def test_reject_invalid_citizen_id_number(self):
         form = RegistrationForm()
-        form.cleaned_data = {'id_number':'12345678a'}
-        self.assertRaises(ValidationError,form.clean_id_number)
-    
+        form.cleaned_data = {'id_number': '12345678a'}
+        self.assertRaises(ValidationError, form.clean_id_number)
+
+    # Unit test #3 - Yana
+    # PM2020T15-40
     def test_reject_duplicate_citizen_id_number(self):
         form = RegistrationForm()
-        form.cleaned_data = {'id_number':'123456780'}
-        self.assertRaises(ValidationError,form.clean_id_number)    
-        
+        form.cleaned_data = {'id_number': '123456780'}
+        self.assertRaises(ValidationError, form.clean_id_number)
+
+    # Unit test #4 - Yana
+    # PM2020T15-40
     def test_validate_secretary_id_number(self):
         form = RegistrationForm()
-        form.cleaned_data = {'id_number':'12345678M'}
+        form.cleaned_data = {'id_number': '12345678M'}
         result = form.clean_id_number()
         self.assertEqual(result, '12345678M')
-        
+
+    # Unit test #5 - Yana
+    # PM2020T15-40
     def test_validate_phone_number(self):
         form = RegistrationForm()
-        form.cleaned_data = {'phone_number':'1549357633'}
+        form.cleaned_data = {'phone_number': '1549357633'}
         result = form.clean_phone_number()
         self.assertEqual(result, '1549357633')
-    
+
+    # Unit test #6 - Yana
+    # PM2020T15-40
     def test_reject_invalid_phone_number(self):
         form = RegistrationForm()
-        form.cleaned_data = {'phone_number':'154935763a'}
-        self.assertRaises(ValidationError,form.clean_phone_number)  
-    
-    
-    
+        form.cleaned_data = {'phone_number': '154935763a'}
+        self.assertRaises(ValidationError, form.clean_phone_number)
+
+    # Unit test #7 - Yana
+    # PM2020T15-40
     def test_validate_first_name(self):
         form = RegistrationForm()
-        form.cleaned_data = {'first_name':'tamar'}
+        form.cleaned_data = {'first_name': 'tamar'}
         result = form.clean_first_name()
         self.assertEqual(result, 'tamar')
-    
+
+    # Unit test #8 - Yana
+    # PM2020T15-40
     def test_reject_invalid_first_name(self):
         form = RegistrationForm()
-        form.cleaned_data = {'first_name':'tamar1'}
-        self.assertRaises(ValidationError,form.clean_first_name) 
-    
+        form.cleaned_data = {'first_name': 'tamar1'}
+        self.assertRaises(ValidationError, form.clean_first_name)
+
+    # Unit test #9 - Yana
+    # PM2020T15-40
     def test_validate_last_name(self):
         form = RegistrationForm()
-        form.cleaned_data = {'last_name':'cohen'}
+        form.cleaned_data = {'last_name': 'cohen'}
         result = form.clean_last_name()
         self.assertEqual(result, 'cohen')
-    
+
+    # Unit test #10 - Yana
+    # PM2020T15-40
     def test_reject_invalid_last_name(self):
         form = RegistrationForm()
-        form.cleaned_data = {'last_name':'cohen1'}
-        self.assertRaises(ValidationError,form.clean_last_name)
-    
+        form.cleaned_data = {'last_name': 'cohen1'}
+        self.assertRaises(ValidationError, form.clean_last_name)
+
     # Integration tests:
-    
+
+    # Integration test #1 - Yana
+    # PM2020T15-40
     def test_register_citizen(self):
         request = HttpRequest()
         request.method = 'POST'
         request.POST = {
-            'id_number':'123456789',
-            'password':'password',
-            'first_name':'firstname',
-            'last_name':'lastname',
-            'phone_number':'0501234350',
-            'email':'test@gmail.com',
-            'address':'1234',
+            'id_number': '123456789',
+            'password': 'password',
+            'first_name': 'firstname',
+            'last_name': 'lastname',
+            'phone_number': '0501234350',
+            'email': 'test@gmail.com',
+            'address': '1234',
         }
+        # function 1 - preform the register
         response = register(request)
-        self.assertIsInstance(response , HttpResponseRedirect)
-        user = Customer.objects.get(username = '123456789')
-        self.assertTrue(user.check_password('password') , 'Failed to verify password.')
-        self.assertEqual(user.first_name,'firstname')
-        self.assertEqual(user.last_name,'lastname')
-        self.assertEqual(user.phone_number,'0501234350')
-        self.assertEqual(user.email,'test@gmail.com')
-        self.assertEqual(user.address,'1234')
-        
+        self.assertIsInstance(response, HttpResponseRedirect)
+
+        # function 2 - Get the registered user
+        user = Customer.objects.get(username='123456789')
+        self.assertTrue(user.check_password('password'), 'Failed to verify password.')
+        self.assertEqual(user.first_name, 'firstname')
+        self.assertEqual(user.last_name, 'lastname')
+        self.assertEqual(user.phone_number, '0501234350')
+        self.assertEqual(user.email, 'test@gmail.com')
+        self.assertEqual(user.address, '1234')
+
         self.assertFalse(user.is_superuser)
         self.assertFalse(user.is_staff)
         self.assertTrue(user.citizen)
         self.assertFalse(user.secretary)
-        
+
+    # Integration test #2 - Yana
+    # PM2020T15-40
     def test_register_secretary(self):
         request = HttpRequest()
         request.method = 'POST'
         request.POST = {
-            'id_number':'12345678M',
-            'password':'password',
-            'first_name':'firstname',
-            'last_name':'lastname',
-            'phone_number':'0501234350',
-            'email':'test@gmail.com',
-            'address':'1234',
+            'id_number': '12345678M',
+            'password': 'password',
+            'first_name': 'firstname',
+            'last_name': 'lastname',
+            'phone_number': '0501234350',
+            'email': 'test@gmail.com',
+            'address': '1234',
         }
+
+        # function 1 - preform the register
         response = register(request)
-        self.assertIsInstance(response , HttpResponseRedirect)
-        user = Customer.objects.get(username = '12345678M')
-        self.assertTrue(user.check_password('password') , 'Failed to verify password.')
-        self.assertEqual(user.first_name,'firstname')
-        self.assertEqual(user.last_name,'lastname')
-        self.assertEqual(user.phone_number,'0501234350')
-        self.assertEqual(user.email,'test@gmail.com')
-        self.assertEqual(user.address,'1234')
-        
+        self.assertIsInstance(response, HttpResponseRedirect)
+
+        # function 2 - get the registered secretary and assert fields.
+        user = Customer.objects.get(username='12345678M')
+        self.assertTrue(
+            user.check_password('password'),
+            'Failed to verify password.'
+        )
+        self.assertEqual(user.first_name, 'firstname')
+        self.assertEqual(user.last_name, 'lastname')
+        self.assertEqual(user.phone_number, '0501234350')
+        self.assertEqual(user.email, 'test@gmail.com')
+        self.assertEqual(user.address, '1234')
+
         self.assertFalse(user.is_superuser)
         self.assertFalse(user.is_staff)
         self.assertTrue(user.secretary)
-        self.assertFalse(user.citizen)    
-        
+        self.assertFalse(user.citizen)
+
+    # Integration test #3 - Yana
+    # PM2020T15-40
     def test_reject_invalid_registration_data(self):
         request = HttpRequest()
         request.method = 'POST'
         request.POST = {
-            'id_number':'12345678b',
-            'password':'password',
-            'first_name':'firstname',
-            'last_name':'lastname',
-            'phone_number':'0501234350',
-            'email':'test@gmail.com',
-            'address':'1234',
+            'id_number': '12345678b',
+            'password': 'password',
+            'first_name': 'firstname',
+            'last_name': 'lastname',
+            'phone_number': '0501234350',
+            'email': 'test@gmail.com',
+            'address': '1234',
         }
+        # function 1 - attempt to register
         response = register(request)
-        self.assertNotIsInstance(response , HttpResponseRedirect)
-        self.assertEqual(Customer.objects.filter(username = '12345678b').count(), 0, 'The user was created.')
-        
-       
-        
-        
-        
+        self.assertNotIsInstance(response, HttpResponseRedirect)
+
+        # function 2 - Make sure user wasn't registered
+        self.assertEqual(Customer.objects.filter(username='12345678b').count(), 0, 'The user was created.')
